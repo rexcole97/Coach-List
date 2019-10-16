ad3b6d16a279ddce9f0efc3a486ff8095e692daa
rexcole97 <54370507+rexcole97@users.noreply.github.com>
2019-10-16 14:31:06 -0500

    Add files via upload

---
+++ Migrations/20191016133530_alldata10.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Authpractice.Migrations
+{
+    public partial class alldata10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coaches",
+                columns: table => new
+                {
+                    Cid = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CFN = table.Column<string>(nullable: true),
+                    CLN = table.Column<string>(nullable: true),
+                    team = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coaches", x => x.Cid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coaches");
+        }
+    }
+}
